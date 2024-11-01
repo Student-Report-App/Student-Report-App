de3d929fe94d53f3596a014824b17bdbdc2a555f
sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>
2024-11-02 01:24:14 +0530

    refactor: Reformat index.js for better readability and handle POST requests
    - Reformatted index.js to improve code readability
    - Handled additional POST requests in index.js
    - Renamed files for better clarity and organization
    
    Signed-off-by: sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
 require("dotenv").config();
 const express = require("express");
-const PORT = process.env.PORT;
-const MONGODB_URI = process.env.MONGODB_URI;
 const mongoose = require("mongoose");
-var ALL_USER_DATA = [];
-
+const bodyParser = require("body-parser");
 const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use("/public", express.static(__dirname + "/public"));
-app.get("/", function (req, res) {
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
 mongoose
-  .connect(MONGODB_URI)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error("Could not Connect to the database", err);
-  });
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not Connect to the database", err));
 
-const dataSchema = new mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
     username: String,
     email: String,
@@ -29,79 +24,49 @@ const dataSchema = new mongoose.Schema(
     year: String,
     branch: String,
   },
-  { versionKey: false }
+  { versionKey: false, collection: "Users" }
 );
 
-const refreshData = async () => {
+User = mongoose.model("User", userSchema);
+
+const fetchAllUsers = async () => {
   try {
-    ALL_USER_DATA = await GET_ALL_USER();
-    //console.log("User data refreshed successfully");
-    return ALL_USER_DATA;
+    const data = await User.find();
+    return data;
   } catch (err) {
-    console.error("Error refreshing data:", err);
+    console.log(err);
   }
 };
 
-const AUTHENTICATION = async (username, password) => {
-  ALL_USER_DATA = await refreshData();
-  try {
-    if (!ALL_USER_DATA) {
-      console.log("No data found");
-      return undefined;
-    }
+// fetchAllUsers()
+//   .then((data) => console.log(data))
+//   .catch((err) => console.error(err));
 
-    let isUser = ALL_USER_DATA.find((user) => user.username === username);
-    if (isUser && isUser.password === password) {
-      //console.log(`User: ${username} successfully authenticated. `);
-      return true;
-    } else {
-      console.log(`Authentication failed for user: ${username}.`);
-      return false;
-    }
-  } catch (err) {
-    console.log("Error in Authentication", err);
-    return undefined;
+app.post("/auth/login", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const record = await User.findOne({ username: username });
+  if (record === null) {
+    console.log("Invalid Username");
+    res.redirect("/error");
+  } else if (password !== record.password) {
+    console.log("Invalid Password");
+    res.redirect("/error");
+  } else {
+    console.log("Authenticated");
+    res.redirect("/dashboard");
   }
-};
+});
 
-const GET_ALL_USER = async () => {
-  const ALL_USER = mongoose.model("Users", dataSchema, "Users"); // Explicitly specify the collection name
-  return await ALL_USER.find(
-    {},
-    { _id: 0, email: 1, username: 1, password: 1, year: 1, branch: 1 }
-  ).lean();
-};
+app.get("/error", (req, res) => {
+  res.sendFile(__dirname + "/views/error.html");
+});
 
-GET_ALL_USER()
-  .then(async (data) => {
-    ALL_USER_DATA = data;
-    //console.log(ALL_USER)
-    app.post("/submit", async (req, res) => {
-      try {
-        const submittedData = req.body;
-        //console.log(submittedData);
-        const isAuthenticated = await AUTHENTICATION(
-          submittedData["username"],
-          submittedData["password"]
-        );
-        console.log(isAuthenticated);
-        if (isAuthenticated) {
-          res.sendFile(__dirname + "/views/Dashboard.html");
-        } else {
-          res.status(500);
-          res.sendFile(__dirname + "/views/error.html");
-        }
-      } catch (error) {
-        console.error("Error processing form submission:", error);
-        res.status(500);
-        res.send("Internal Server Error");
-      }
-    });
-  })
-  .catch((err) => {
-    console.log("there as an error ", err);
-  });
+app.get("/dashboard", (req, res) => {
+  res.sendFile(__dirname + "/views/dashboard.html");
+});
 
+const PORT = process.env.PORT ? process.env.PORT : 3000;
 app.listen(PORT, () => {
   console.log("Your app is listening on port " + PORT);
 });
