d8b52732b9eb7254934674e29f7fe7e359f5c32f
prashilthul <prashil1411@gmail.com>
2024-11-02 00:24:41 +0530

    Completed the login portion of the project - Implemented login logic on the login page - Made minor adjustments in index.js - Added Dashboard.html and error.html files - Created Dashboard-style.css and error-style.css for styling
    Signed-off-by: prashilthul <70517473+prashilthul@users.noreply.github.com>

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const mongoose = require("mongoose");
 var ALL_USER_DATA = [];
 
 const app = express();
-app.use("/public", express.static(process.cwd() + "/public"));
+app.use(express.urlencoded({ extended: true }));
+app.use("/public", express.static(__dirname + "/public"));
 app.get("/", function (req, res) {
-  res.sendFile(process.cwd() + "/views/index.html");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 mongoose
@@ -34,7 +35,7 @@ const dataSchema = new mongoose.Schema(
 const refreshData = async () => {
   try {
     ALL_USER_DATA = await GET_ALL_USER();
-    console.log("User data refreshed successfully");
+    //console.log("User data refreshed successfully");
     return ALL_USER_DATA;
   } catch (err) {
     console.error("Error refreshing data:", err);
@@ -51,7 +52,7 @@ const AUTHENTICATION = async (username, password) => {
 
     let isUser = ALL_USER_DATA.find((user) => user.username === username);
     if (isUser && isUser.password === password) {
-      console.log(`User: ${username} successfully authenticated. `);
+      //console.log(`User: ${username} successfully authenticated. `);
       return true;
     } else {
       console.log(`Authentication failed for user: ${username}.`);
@@ -63,9 +64,9 @@ const AUTHENTICATION = async (username, password) => {
   }
 };
 
-const GET_ALL_USER = () => {
+const GET_ALL_USER = async () => {
   const ALL_USER = mongoose.model("Users", dataSchema, "Users"); // Explicitly specify the collection name
-  return ALL_USER.find(
+  return await ALL_USER.find(
     {},
     { _id: 0, email: 1, username: 1, password: 1, year: 1, branch: 1 }
   ).lean();
@@ -75,9 +76,27 @@ GET_ALL_USER()
   .then(async (data) => {
     ALL_USER_DATA = data;
     //console.log(ALL_USER)
-    const isAuthenticated = await AUTHENTICATION("user", "test");
-    //console.log(data)
-    //console.log(isAuthenticated)
+    app.post("/submit", async (req, res) => {
+      try {
+        const submittedData = req.body;
+        //console.log(submittedData);
+        const isAuthenticated = await AUTHENTICATION(
+          submittedData["username"],
+          submittedData["password"]
+        );
+        console.log(isAuthenticated);
+        if (isAuthenticated) {
+          res.sendFile(__dirname + "/views/Dashboard.html");
+        } else {
+          res.status(500);
+          res.sendFile(__dirname + "/views/error.html");
+        }
+      } catch (error) {
+        console.error("Error processing form submission:", error);
+        res.status(500);
+        res.send("Internal Server Error");
+      }
+    });
   })
   .catch((err) => {
     console.log("there as an error ", err);
