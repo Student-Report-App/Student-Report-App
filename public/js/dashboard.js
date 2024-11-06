12e62ae369e5839bae539e7b393c7f137b0ccf46
prashilthul <prashil1411@gmail.com>
2024-11-06 23:38:15 +0530

    Feat:Implemented the use of API for fetching the current classes
    Signed-off-by: prashilthul <70517473+prashilthul@users.noreply.github.com>

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -24,6 +24,7 @@ currentDate.textContent = new Date().toDateString();
 
 const today = days[new Date().getDay() - 1];
 const classItems = Array.from(document.querySelectorAll(".class-item"));
+
 fetch(`/api/timetable/CSE/${today}`)
   .then((response) => response.json())
   .then((data) => {
@@ -36,4 +37,74 @@ fetch(`/api/timetable/CSE/${today}`)
         item.style.backgroundColor = "#16E838";
       }
     });
+
+    const hour = new Date().getHours();
+    console.log(hour);
+
+    let timeSlot;
+    switch (true) {
+      case (9 <= hour && hour < 10):
+        timeSlot = 1;
+        break;
+      case (10 <= hour && hour < 11):
+        timeSlot = 2;
+        break;
+      case (11 <= hour && hour < 12):
+        timeSlot = 3;
+        break;
+      case (12 <= hour && hour < 13):
+        timeSlot = 4;
+        break;
+      case (14 <= hour && hour < 15):
+        timeSlot = 5;
+        break;
+      case (15 <= hour && hour < 16):
+        timeSlot = 6;
+        break;
+      case (16 <= hour && hour < 17):
+        timeSlot = 7;
+        break;
+      case (17 <= hour && hour < 18):
+        timeSlot = 8;
+        break;
+      default:
+        timeSlot = null;  
+        break;
+    }
+
+    let currentClass;
+    switch (timeSlot) {
+      case 1:
+        currentClass = document.getElementById("1");
+        break;
+      case 2:
+        currentClass = document.getElementById("2");
+        break;
+      case 3:
+        currentClass = document.getElementById("3");
+        break;
+      case 4:
+        currentClass = document.getElementById("4");
+        break;
+      case 5:
+        currentClass = document.getElementById("5");
+        break;
+      case 6:
+        currentClass = document.getElementById("6");
+        break;
+      case 7:
+        currentClass = document.getElementById("7");
+        break;
+      case 8:
+        currentClass = document.getElementById("8");
+        break;
+      default:
+        currentClass = null;  
+        break;
+    }
+
+    if (currentClass && currentClass.innerText !== "Free") {
+      currentClass.style.color = "#fff"; 
+      currentClass.style.backgroundColor = "#2c3e50"; 
+    }
   });
