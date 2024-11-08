ee637903cb21d775bd76fda6462d0b0d1054cb2d
sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>
2024-11-08 15:26:22 +0530

    feat: Add toggle to view and hide password
    Signed-off-by: sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>

--- public/js/index.js
+++ public/js/index.js
@@ -65,3 +65,17 @@ const handleLogin = async (e) => {
 };
 
 loginBtn.addEventListener("click", handleLogin);
+
+document.getElementById('toggle-password').addEventListener('click', function() {
+  const passwordInput = document.getElementById('password');
+  const icon = this.querySelector('i');
+  if (passwordInput.type === 'password') {
+    passwordInput.type = 'text';
+    icon.classList.remove('fa-eye');
+    icon.classList.add('fa-eye-slash');
+  } else {
+    passwordInput.type = 'password';
+    icon.classList.remove('fa-eye-slash');
+    icon.classList.add('fa-eye');
+  }
+});
